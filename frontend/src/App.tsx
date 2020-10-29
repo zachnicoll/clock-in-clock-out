31b51322459819e2abae839b2ca3ca97af5015b8
Zach <zach.nicoll55@gmail.com>
2020-10-29 20:53:31 +1000

    Changed url to prod url.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import Axios from 'axios';
 import { AuthProvider } from './Helpers/AuthContext';
 import AppRouter from './AppRouter';
 
-Axios.defaults.baseURL = 'http://localhost:8000/';
+Axios.defaults.baseURL = 'http://clockinout.net/';
 
 function App() {
   return (
--- frontend/src/Pages/LandingPage.tsx
+++ frontend/src/Pages/LandingPage.tsx
@@ -6,10 +6,10 @@ export default function LandingPage({ navigation }: any) {
     const history = useHistory();
 
     return (
-        <div className="page-container">
-            <h1 style={{ fontWeight: "normal", fontSize: "64px", textAlign: "center" }}>Clock-in/Clock-out</h1>
+        <div className="page-container h-space">
+            <h1 style={{ fontWeight: "normal", fontSize: "48px", textAlign: "center" }}>Clock-in/Clock-out</h1>
             <hr />
-            <h2>Track every task and ticket in one place.</h2>
+            <h2 style={{ textAlign: "center" }}>Track every task and ticket in one place.</h2>
 
             <Login />
 
